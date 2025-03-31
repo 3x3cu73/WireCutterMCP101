99337975a1d1228db23f9d01c30496203be27d54
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-04-01 02:08:09 +0530

    drag and drop order Change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,12 @@ import React, {useEffect, useState} from "react";
 import {closestCorners, DndContext} from "@dnd-kit/core";
 import {Column} from "../components/jobList.jsx";
 import Navigation from "../components/Navigation.jsx";
+import {arrayMove} from "@dnd-kit/sortable";
 
 
 export const Dashboard = () => {
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
+    const [jsonData,setJsonData] = useState([]);
     useEffect(() => {
         fetchData();
     }, []);
@@ -17,31 +19,50 @@ export const Dashboard = () => {
             }
 
             const result = await response.json(); // Parse JSON data
-            setData(result["jobs"]);
+            setJsonData(result["jobs"].map((item) => ({
+                jobid: item[0],
+                id: item[1],
+                status: item[2],
+                user: item[3],
+                a: item[4],
+                b: item[5],
+                c: item[6],
+                timestamp: item[7],
+                description: item[8],
+                title: item[9]
+            })));
         }
         catch(error){
             console.log(error);
         }
     }
 
-    const jsonData = data.map((item) => ({
-        jobid: item[0],
-        id: item[1],
-        status: item[2],
-        user: item[3],
-        a: item[4],
-        b: item[5],
-        c: item[6],
-        timestamp: item[7],
-        description: item[8],
-        title: item[9]
-    }));
+
+
+
+
+    const handleDragEnd = event => {
+        const {active,over}=event
+        if (active.id===over.id) return null;
+
+        setJsonData(jsonData=>{
+            const oldPosition=jsonData.findIndex(task => task.id === active.id)
+            const newPosition=jsonData.findIndex(task => task.id === over.id)
+
+            return arrayMove(jsonData,oldPosition,newPosition)
+
+        })
+
+    }
+
+    // console.log(jsonData);
+
     return (
         <>
             <Navigation activity={[true,false,false,false]}/>
             {/*<Example items={data} />*/}
         <div>Task</div>
-        <DndContext collisionDetection={closestCorners}>
+        <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
             <Column tasks={jsonData}/>
         </DndContext>
         </>
