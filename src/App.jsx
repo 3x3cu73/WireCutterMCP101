d491d5346ff32c63a1e7ebc71c2ab6c26753f368
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-04-02 02:00:50 +0530

    Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import './App.css'
-import {Dashboard} from "./pages/Dashboard.jsx";
-import {Route, Routes} from "react-router-dom";
+import './App.css';
+import { Dashboard } from "./pages/Dashboard.jsx";
+import { Route, Routes } from "react-router-dom";
 import Team from "./pages/Team.jsx";
 
-
 function App() {
-return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/Team" element={<Team />} />
-    </Routes>
-)
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-50">
+            {/* Main Application Routes */}
+            <Routes>
+                {/* Dashboard Route */}
+                <Route path="/" element={<Dashboard />} />
+
+                {/* Team Route */}
+                <Route path="/Team" element={<Team />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+// Mobile-first responsive configuration
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter>
-          <App/>
-
-      </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <div className="min-h-screen flex flex-col">
+                <App className="flex-1 w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8" />
+            </div>
+        </BrowserRouter>
+    </StrictMode>
 )
