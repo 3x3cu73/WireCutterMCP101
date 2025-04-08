d5a0c89c17f17e915d481d13f5a8a3188ab03645
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-04-09 03:11:38 +0530

    status

--- src/components/jobList.jsx
+++ src/components/jobList.jsx
@@ -2,7 +2,7 @@ import {SortableContext, verticalListSortingStrategy} from "@dnd-kit/sortable";
 import {Task} from "./task.jsx";
 
 export const Column=({tasks,openModal})=>{
-    const className="m-3 w-full md:w-80 min-h-32 bg-blue-100 border border-blue-300 rounded-lg p-4 shadow-md flex-shrink-0"
+    const className="m-3 w-128 min-h-32 min-w-32 flex-col bg-blue-200 border border-blue-600 rounded-lg p-4"
 
 
 
--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,47 +1,41 @@
-import {CSS} from "@dnd-kit/utilities";
-import {useSortable} from "@dnd-kit/sortable";
+// src/components/Task.jsx (Old Code)
+import { CSS } from "@dnd-kit/utilities";
+import { useSortable } from "@dnd-kit/sortable";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as faIcons from '@fortawesome/free-solid-svg-icons';
-import {deleteTask} from "../services/deleteTask.jsx";
+import { deleteTask } from "../services/deleteTask.jsx";
 
-export const Task=({id,task,openModal})=>{
-    const {attributes , listeners,setNodeRef,transform ,transition } = useSortable({id});
+export const Task = ({ id, task, openModal }) => {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
-    const style ={
+    const style = {
         transition,
-        transform:CSS.Transform.toString(transform),
-    }
+        transform: CSS.Transform.toString(transform),
+    };
 
-    const className=" m-3  bg-white border border-blue-300 rounded-lg p-2"
-
+    const className = " m-3  bg-white border border-blue-300 rounded-lg p-2";
 
     return <div ref={setNodeRef} {...attributes} {...listeners} className={className} style={style}>
         <span className="text-blue-400 font-bold text-lg">{id}.   </span>
         <span className="text-blue-400 font-bold text-lg">{task.title}</span>
-        <br/>
+        <br />
         <div className="flex justify-between items-center">
             <span className="text-green-500">Quantity: {task.a}</span>
             <span className="text-red-500">Length: {task.b}</span>
             <span className="text-orange-500">Stripping: {task.c}</span>
 
-
-
             <button onClick={() => openModal(task)}>
                 <FontAwesomeIcon
                     icon={faIcons.faPencil}
                     className="text-blue-500 text-xl cursor-pointer hover:text-blue-600"
-                    // Pass task to openModal
-                    />
-        </button>
+                />
+            </button>
             <button onClick={() => deleteTask(task)}>
                 <FontAwesomeIcon
                     icon={faIcons.faTrashCan}
                     className="text-red-500 text-xl cursor-pointer hover:text-red-600"
-                    // Pass task to openModal
                 />
             </button>
         </div>
-
-
-    </div>
-}
+    </div>;
+};
