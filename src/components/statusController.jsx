e41c49a258e60e29df6f5d2f42dbd39a5e385372
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-04-13 03:56:44 +0530

    deleteStatus Change

--- src/components/statusController.jsx
+++ src/components/statusController.jsx
@@ -57,6 +57,7 @@ function BeautifulStatusDisplay() {
             const response = await fetch('https://vps.sumitsaw.tech/api/mcp101/status/last', {
                 headers: { 'accept': 'application/json' },
                 cache: 'no-store',
+
             });
 
             if (!response.ok) {
