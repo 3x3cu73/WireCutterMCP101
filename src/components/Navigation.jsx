c7b99ce7c760e7070dfe2161b9aa476531f6dfcd
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-04-13 03:43:14 +0530

    deleteStatus Change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -95,7 +95,7 @@ export default function Navigation({ activity = [true, false, false, false] }) {
                                                 <span className="sr-only">Open user menu</span>
                                                 <img
                                                     alt="User avatar" // More descriptive alt text
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                    src="/sarkarSir.png"
                                                     className="size-8 rounded-full"
                                                 />
                                             </MenuButton>
